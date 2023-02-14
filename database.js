a6294b3b36e2d3b1b1579748c2b326ca9595d3e3
Ivan <ivanyu327@gmail.com>
2023-02-14 02:07:57 -0500

    create functions to initalize database and load dummy data

--- database.js
+++ database.js
@@ -1,24 +1,104 @@
-var sqlite3 = require("sqlite3").verbose();
+import sqlite3 from "sqlite3";
+// import fetch from "node-fetch";
+import { USER_DATA } from "./HTN_2023_BE_Challenge_Data.js";
 
 const DATABASE_SOURCE = "db.sqlite";
 
-let db = new sqlite3.Database(DATABASE_SOURCE, (err) => {
+const db = new sqlite3.Database(DATABASE_SOURCE, (err) => {
   if (err) {
-    console.error(err.message);
+    console.error(`Error in initizlizing database. Error: ${err}`);
     throw err;
-  } else {
-    console.log("Connected to the SQLite database.");
-    db.run(``, (err) => {
-      if (err) {
-        console.log("Table already exists");
-      } else {
-        console.log("Creating entries");
-        var insert = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
-        db.run(insert, []);
-        db.run(insert, []);
-      }
-    });
   }
 });
 
-module.exports = db;
+console.log("Connected to the SQLite database.");
+
+db.serialize(() => {
+  // db.run(`DROP TABLE IF EXISTS user;`, (err) => {
+  //   if (err) console.log(`Failed to drop skill table if exists. Error: ${err}`);
+  //   else console.log("User table dropped");
+  // });
+
+  // db.run(`DROP TABLE IF EXISTS skill;`, (err) => {
+  //   if (err) console.log(`Failed to drop skill table if exists. Error: ${err}`);
+  //   else console.log("Skill table dropped");
+  // });
+
+  db.run(
+    `
+    CREATE TABLE user (
+      user_id integer PRIMARY KEY AUTOINCREMENT,
+      name text NOT NULL,
+      company text, 
+      email text UNIQUE,
+      phone text UNIQUE, 
+      CONSTRAINT email_unique UNIQUE (email),
+      CONSTRAINT phone_unique UNIQUE (phone)
+    )
+  `,
+    (err) => {
+      if (err) {
+        console.log(`User table already exists. Error: ${err}`);
+      } else {
+        console.log("User table created.");
+      }
+    }
+  );
+
+  db.run(
+    `
+    CREATE TABLE skill (
+      skill_id integer PRIMARY KEY AUTOINCREMENT,
+      skill text NOT NULL,
+      rating int NOT NULL, 
+      user_id integer NOT NULL,
+      FOREIGN KEY (user_id)
+        REFERENCES user (user_id)
+    )
+  `,
+    (err) => {
+      if (err) {
+        console.log(`Skills table already exists. Error: ${err}`);
+      } else {
+        console.log("Skills table created.");
+        populateTables();
+      }
+    }
+  );
+});
+
+const populateTables = () => {
+  let BUSER_DATA = USER_DATA.slice(0, 5);
+  console.log(`Populating the database with ${BUSER_DATA.length} objects`);
+
+  const userInsert =
+    "INSERT INTO user (name, company, email, phone) VALUES (?,?,?,?) RETURNING user_id";
+
+  const skillInsert =
+    "INSERT INTO skill (user_id, skill, rating) VALUES (?,?,?)";
+
+  BUSER_DATA.forEach((user) => {
+    db.serialize(() => {
+      const userValues = [user.name, user.company, user.email, user.phone];
+
+      db.run(userInsert, userValues);
+
+      let userId;
+      db.get(`SELECT last_insert_rowid()`, (err, row) => {
+        if (err) console.log(`Insert failed for ${user}. \nError: ${err}`);
+        userId = row["last_insert_rowid()"];
+        // console.log(userId);
+
+        db.parallelize(() => {
+          user.skills.forEach((skill) => {
+            // console.log(skill);
+            const skillValues = [userId, skill.skill, skill.rating];
+            db.run(skillInsert, skillValues);
+          });
+        });
+      });
+    });
+  });
+};
+
+export default db;
--- server.js
+++ server.js
@@ -1,17 +1,47 @@
-var express = require("express");
-var app = express();
+import express from "express";
+import db from "./database.js";
+
+const app = express();
 
 // Server port
-var PORT = 8000;
+const PORT = 8000;
 // Start server
 app.listen(PORT, () => {
-  console.log("Server running on port %PORT%".replace("%PORT%", PORT));
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Root endpoint
 app.get("/", (req, res, next) => {
   res.json({ message: "Ok" });
 });
+
+//All Users Endpoint
+app.get("/api/users", (req, res, next) => {
+  const sql = "SELECT * FROM user";
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+//User Information Endpoint
+
+//Updating User Data Endpoint
+
+//Skills Endpoints
+// These endpoint should show a list of skills and aggregate info about them. Note that in the context of your hackathon, users do not gain/lose skills very often.
+
+// Try to implement the following (in SQL or with an ORM):
+
+// Number of users with each skill (frequency)
+// Query parameter filtering - minimum/maximum frequency
 
 // Insert here other API endpoints
 
